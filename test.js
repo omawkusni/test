ba4465e29689d37068c5401206f73e4d3df251de
omaw kusni <t0o0k0o0@hotmail.com>
2022-08-11 06:18:31 +0900

    キャンバスを追加

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
 'use strict';
-let element = document.getElementById('textin');
-console.log(element.value)
+var canvas = document.getElementById('testcanvas');
+
+if (canvas.getContext) {
+  var ctx = canvas.getContext('2d');
+  console.log("大丈夫");
+  ctx.fillStyle = 'black';
+
+// 四角形を描画するメソッドfillRect()
+ctx.fillRect(10, 10, 50, 50);
+ctx.fillStyle = 'red';
+ctx.fillRect(70, 10, 50, 50);
+  // 描画するコードをここに
+} else {
+  console.log("エラー");
+  // キャンバスに未対応の場合のコードをここに
+}
