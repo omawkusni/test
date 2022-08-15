a4d8fa7ca32c1531e9bd20ac63c97ad7a8d1e79f
omaw kusni <t0o0k0o0@hotmail.com>
2022-08-15 19:41:27 +0900

    配列して色変換前

--- test.js
+++ test.js
@@ -3,35 +3,65 @@ const ConversionButton = document.getElementById('bConversion');
 const canvas = document.getElementById('testcanvas');
 const outputArea = document.getElementById('outputArea')
 const inputText = document.getElementById('textin')
-if (canvas.getContext) {
-  var ctx = canvas.getContext('2d');
-  console.log("大丈夫");
+const arrYouon = ['ぁ','ぃ','ぅ','ぇ','ぉ','ゃ','ゅ','ょ','ゎ','ゕ','ゖ']
+let arrBoin = [
+  [["あ","か","さ","た","な","は","ま","や","ら","わ"], "red"],
+  [["い","き","し","ち","に"],"blue"],
+  [["う","く","す","つ","ぬ"],"yellow"],
+  [["え","け","せ","て","ね"],"purple"],
+  [["お","こ","そ","と","の"],"orange"],
+  ];
 
-
-// 四角形を描画するメソッドfillRect()
-ctx.fillStyle = 'black';
-ctx.fillRect(0, 0, 50, 50);
-ctx.fillStyle = 'red';
-ctx.fillRect(55, 0, 50, 50);
-  // 描画するコードをここに
-} else {
-  console.log("エラー");
-  // キャンバスに未対応の場合のコードをここに
-}
-
-
+//変換ボタンを押したときの出力
 ConversionButton.onclick = () => {
-  const alltext = inputText.value;
+  // テキストが空の時は処理を終了する
   if (inputText.length === 0) {
-    // テキストが空の時は処理を終了する
     return;
   }
+  //テキストを1文字づつ配列にいれる
+  const alltext = inputText.value;
+  let arrTest = alltext.split('');
+  //拗音、撥音を分けなおして1音ずつに配列しなおす
+  let strMax = alltext.length; //文字数
+  const arrFormated = formatSingleWord(strMax,arrTest).split(',');
+
+  console.log(arrBoin[0][1]);
+  console.log(arrFormated);
 
   // 診断結果表示エリアの作成
-
   const paragraph = document.createElement('p');
   paragraph.innerText = alltext;
   outputArea.appendChild(paragraph);
 
+}
 
+function formatSingleWord(maxlength,arrTest) {
+  let buf = ''
+  for (let i = 0; i <= maxlength - 1; i++) {
+    if (buf === '' ) {
+      buf = arrTest[i]
+    } else if ( arrTest[i] === 'っ') {
+      buf = buf + arrTest[i];
+    } else if (arrYouon.includes(arrTest[i])) {
+      buf = buf + arrTest[i];
+    } else {
+      buf = buf +',' + arrTest[i];
+    }
+  }
+  return buf;
+}
+
+if (canvas.getContext) {
+  
+  var ctx = canvas.getContext('2d');
+  console.log("大丈夫");
+  // 四角形を描画するメソッドfillRect()
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, 50, 50);
+  ctx.fillStyle = 'red';
+  ctx.fillRect(55, 0, 50, 50);
+    // 描画するコードをここに
+} else {
+  console.log("エラー");
+  // キャンバスに未対応の場合のコードをここに
 }
